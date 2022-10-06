50dfe9b4fddb5c58873fe39f85825a8407a581b6
sebastian.revuelta <sebastian.revuelta@thalesaleniaspace.com>
2022-10-06 17:06:08 +0200

    add more example reports

---
+++ load-demo-data.sh
@@ -0,0 +1,2 @@
+python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:5000 --application vulnerable-flask-app --report ./examples/vulnerable-flask-app/bandit-report.json --scan bandit
+python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:5000 --application vulnerable-flask-app --report ./examples/vulnerable-flask-app/safety-report.json --scan safety
