f3e6a5c7e214f18ef1ed430c759032cbe80c4ea2
sebastian.revuelta <sebastian.revuelta@thalesaleniaspace.com>
2022-10-11 16:47:38 +0200

    add new demo data

--- load-demo-data.sh
+++ load-demo-data.sh
@@ -8,3 +8,8 @@ python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:500
 python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:5000 --application test --report ./reports/test_projects/coverity-report.json --scan coverity
 python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:5000 --application juice-shop --report ./reports/juice-shop/semgrep-report.json --scan semgrep
 python3 ./client/dash4ast-cli-import-local-report.py --host http://localhost:5000 --application juice-shop --report ./reports/juice-shop/owaspzap-report.xml --scan owaspzap
+python3 ./client/dash4ast-cli-import-blackduck-report.py --host http://localhost:5000 --application-tool demo/webgoat --application-dash4ast webgoat --url-tool $BLACKDUCK_URL --api-key-tool $BLACKDUCK_API_TOKEN
+python3 ./client/dash4ast-cli-import-blackduck-report.py --host http://localhost:5000 --application-tool demo/dash4ast --application-dash4ast dash4ast --url-tool $BLACKDUCK_URL --api-key-tool $BLACKDUCK_API_TOKEN
+python3 ./client/dash4ast-cli-import-blackduck-report.py --host http://localhost:5000 --application-tool demo/vulnerable-flask-app --application-dash4ast vulnerable-flask-app --url-tool $BLACKDUCK_URL --api-key-tool $BLACKDUCK_API_TOKEN
+python3 ./client/dash4ast-cli-import-blackduck-report.py --host http://localhost:5000 --application-tool demo/chess --application-dash4ast chess --url-tool $BLACKDUCK_URL --api-key-tool $BLACKDUCK_API_TOKEN
+python3 ./client/dash4ast-cli-import-blackduck-report.py --host http://localhost:5000 --application-tool demo/juice-shop --application-dash4ast juice-shop --url-tool $BLACKDUCK_URL --api-key-tool $BLACKDUCK_API_TOKEN
