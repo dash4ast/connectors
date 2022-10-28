12737e0cd2063c41bc786ef800645e9afcd739f2
sebastian.revuelta <sebastian.revuelta@thalesaleniaspace.com>
2022-10-28 12:56:37 +0200

    add improvements to coverity connector

--- connectors/src/connectors/endpoints/coverity_import.py
+++ connectors/src/connectors/endpoints/coverity_import.py
@@ -2,6 +2,7 @@ import sqlalchemy
 from flasgger import swag_from
 from flask import Blueprint, request, abort, jsonify, make_response
 from marshmallow import Schema, fields
+from psycopg2 import OperationalError
 from sqlalchemy.exc import IntegrityError
 
 from connectors.db import UtilDb
@@ -126,10 +127,42 @@ def extract():
     })
 
 
+def extract_test():
+
+    report = open('../../../test/webgoat-report-coverity.json', 'r').read()
+    content = json.loads(report)
+
+    dash4ast_application = 'test33'
+    now = datetime.now()
+
+    db_session = PostgreDbClient().get_client()
+    db_session()
+    db_session.flush()
+    try:
+        for issue in content['issues']:
+            print(issue['mergeKey'])
+            vulnerability = create_vulnerability(issue, dash4ast_application, now)
+            UtilDb.add_vulnerability(db_session, vulnerability)
+    except IntegrityError:
+        print('IntegrityError key: ' + issue['mergeKey'])
+    except OperationalError:
+        print('OperationalError key: ' + issue['mergeKey'])
+    db_session.remove()
+
+    new_vulnerabilities = len(content['issues'])
+
+    # update analysis table
+    analysis = UtilDb.create_analysis(dash4ast_application, 'sast', now)
+    UtilDb.add_analysis(db_session, analysis)
+
+    print("successfully extraction")
+
+
 def create_vulnerability(issue, application_name, now):
     vulnerability = Vulnerability()
-    vulnerability.vulnerability_id = hashlib.md5(str(issue['mergeKey']+issue['strippedMainEventFilePathname']+
-                                                     str(issue['mainEventLineNumber'])).encode()).hexdigest()
+    vulnerability.vulnerability_id = hashlib.md5(str(issue['mergeKey'] + issue['strippedMainEventFilePathname'] +
+                                                     str(issue['mainEventLineNumber']) +
+                                                     str(issue['occurrenceNumberInMK'])).encode()).hexdigest()
     vulnerability.description = issue['checkerProperties']['subcategoryShortDescription']
     vulnerability.tool = 'coverity'
     vulnerability.analysis_type = 'sast'
@@ -147,20 +180,23 @@ def create_vulnerability(issue, application_name, now):
 
 
 def test():
-    report = open('../../../test/coverity-report.json', 'r').read()
+    report = open('../../../test/webgoat-report-coverity.json', 'r').read()
     content = json.loads(report)
     print(len(content['issues']))
     now = datetime.now()
     counter = 0
     for issue in content['issues']:
         counter = counter + 1
+        print('----------------------')
         print(counter)
         print_vulnerability(issue, 'test-app', now)
 
 
 def print_vulnerability(issue, application_name, now):
     vulnerability = Vulnerability()
-    print(hashlib.md5(str(issue['mergeKey']+issue['strippedMainEventFilePathname']+str(issue['mainEventLineNumber'])).encode()).hexdigest())
+    print(hashlib.md5(str(issue['mergeKey'] + issue['strippedMainEventFilePathname'] +
+                                                     str(issue['mainEventLineNumber']) +
+                                                     str(issue['occurrenceNumberInMK'])).encode()).hexdigest())
     print(issue['checkerProperties']['subcategoryShortDescription'])
     print('coverity')
     print('sast')
@@ -186,4 +222,5 @@ def print_vulnerability(issue, application_name, now):
 
 if __name__ == '__main__':
     extract()
+    # extract_test()
     # test()
