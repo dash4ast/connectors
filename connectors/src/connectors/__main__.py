2076df3b66541d5ecafc9a4088c631426fa11323
dash4ast-community <support@dash4ast.com>
2023-01-30 12:49:44 +0100

    adding small changes to test mr comments

--- connectors/src/connectors/__main__.py
+++ connectors/src/connectors/__main__.py
@@ -4,4 +4,4 @@ from waitress import serve
 if __name__ == "__main__":
     # Deployment on Weitress
     serve(app, host='0.0.0.0', port=5000)
-    # Should be it changed to 192.168.0.1
+    # Should be it changed to 192.168.0.1?
